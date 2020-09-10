8beaca10f557f187499f07dbd9097aad57f0cb9f
Drew Graham <dmg9626@gmail.com>
2020-09-10 18:31:49 -0400

    - circular pulse appears where player spawns boids - grouping all boid instances under Boids parent object

--- Assets/Scripts/Boid/BoidSpawner.cs
+++ Assets/Scripts/Boid/BoidSpawner.cs
@@ -8,13 +8,15 @@ public class BoidSpawner : Singleton<BoidSpawner>
     private BoidManager boidManager => BoidManager.Instance;
 
     /// <summary>
+    /// Parent object that holds all boids spawned
+    /// </summary>
+    private GameObject boidContainer;
+
+    /// <summary>
     /// Reference to boid prefab (used for instantiation)
     /// </summary>
     [SerializeField]
     private Boid boidPrefab;
-
-    [SerializeField]
-    private Button clickDetection;
 
     float distanceFromCamera = 10f;
 
@@ -24,15 +26,39 @@ public class BoidSpawner : Singleton<BoidSpawner>
     [SerializeField]
     private int startingBoids;
 
+    /// <summary>
+    /// UI element that detects when user clicks screen
+    /// </summary>
+    [SerializeField]
+    private Button clickDetection;
+
+    #region TapIndicator
+    [Header("Tap Indicator Settings")]
+
+    [SerializeField]
+    private GameObject tapIndicatorPrefab;
+
+    /// <summary>
+    /// Speed at which tap indicator animates
+    /// </summary>
+    [Range(0, 2)]
+    [SerializeField]
+    private float tapIndicatorSpeed;
+
+    [SerializeField]
+    private Color tapIndicatorColor;
+    #endregion
+
+
     // Start is called before the first frame update
     void Start()
     {
-        // Instantiate boids in scene
+        // Create container to hold all spawned boids
+        boidContainer = new GameObject("Boids");
+
+        // Instantiate boids in scene at random positions
         for (int i = 0; i < startingBoids; i++)
-        {
-            // Spawn bird at random position
-            Boid boid = SpawnBoid(new Vector3(Random.value, Random.value, distanceFromCamera));
-        }
+            SpawnBoid(new Vector3(Random.value, Random.value, distanceFromCamera));
 
         // Spawn boid at click position if haven't exceeded max count
         clickDetection.onClick.AddListener(() => {
@@ -42,7 +68,11 @@ public class BoidSpawner : Singleton<BoidSpawner>
                 Vector3 viewportPos = Camera.main.ScreenToViewportPoint(screenPos);
                 viewportPos.z = distanceFromCamera;
 
+                // Spawn boid
                 Boid boid = SpawnBoid(viewportPos);
+
+                // Create circular pulse at spawn position
+                StartCoroutine(CreatePulse(boid.transform.position));
             }
         });
     }
@@ -54,7 +84,7 @@ public class BoidSpawner : Singleton<BoidSpawner>
     /// <returns></returns>
     public Boid SpawnBoid(Vector3 viewportPosition)
     {
-        Boid boid = Instantiate(boidPrefab);
+        Boid boid = Instantiate(boidPrefab, boidContainer.transform);
         boid.name = "Boid " + BoidManager.Instance.numberOfBoids;
 
         // Set given position
@@ -63,5 +93,38 @@ public class BoidSpawner : Singleton<BoidSpawner>
 
         boidManager.numberOfBoids++;
         return boid;
+    }
+
+    // TODO: consider optimizing this to use an object pool
+    /// <summary>
+    /// Coroutine animation that indicates position tapped by user
+    /// </summary>
+    /// <param name="tapPosition"></param>
+    /// <returns></returns>
+    private IEnumerator CreatePulse(Vector3 tapPosition)
+    {
+        // Set tap indicator to initial state (active = true, at tap position, scale = 0)
+        GameObject tapIndicator = Instantiate(tapIndicatorPrefab);
+        tapIndicator.transform.position = tapPosition;
+        tapIndicator.transform.localScale = Vector3.zero;
+
+        // Set starting color
+        SpriteRenderer spriteRenderer = tapIndicator.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = tapIndicatorColor;
+        Color color = spriteRenderer.color;
+
+        for(float t = 0; t < 1; t += Time.deltaTime * tapIndicatorSpeed) {
+            // Move scale value up the curve (grow from 0 to 1)
+            tapIndicator.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, t);
+            
+            // Move color alpha value down the curve (shrink from 1 to 0)
+            color.a = Mathf.Lerp(1, 0, t);
+            spriteRenderer.color = color;
+
+            yield return null;
+        }
+
+        // Destroy when animation finished
+        Destroy(tapIndicator);
     }
 }
